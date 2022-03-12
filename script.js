e7998c92d7d0f352f0a3d6f1ef2b83effc895e2a
HuziMuzi <laif17860@mail.ru>
2022-03-12 21:57:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-function sum (numbers) {
+function sum(numbers) {
 	"use strict";
-  let out = 0
-	for(let i = 0; i < numbers.length; i++){
+	let out = 0
+	for (let i = 0; i < numbers.length; i++) {
 		out += numbers[i]
 	}
-	return out	
+	return out
 };
 
-args = [78,56,232,12,8]
+args = [78, 56, 232, 12, 8]
 class SmallestIntegerFinder {
 	findSmallestInt(args) {
-    let out = args[0];
-		for(let i = 0; i < args.length; i++){
-			if(args[i] < out) out = args[i]
+		let out = args[0];
+		for (let i = 0; i < args.length; i++) {
+			if (args[i] < out) out = args[i]
 		}
-    return out
+		return out
 	}
- }
+}
 
- function paperwork(n, m) {
+function paperwork(n, m) {
 	let res
-	  if(n < 0 || m < 0){
-		 res = 0;
-	  } else {
-		 res = n * m
-	  }
-	 return res
- }
- 
+	if (n < 0 || m < 0) {
+		res = 0;
+	} else {
+		res = n * m
+	}
+	return res
+}
+
+// function paperwork(n, m) {
+//   return n > 0 && m > 0 ? n * m : 0
+// }
+let arr = [4, '5', '7']
+function sumMix(x) {
+	let res = 0;
+	for (let i = 0; i < x.length; i++) {
+		res += Number(x[i])
+	}
+	return res
+}
+
+// console.log(sumMix(arr))
+
+// function sumMix(x){
+// 	return x.map(a => +a).reduce()
+// }
+
+function makeNegative(num) {
+	// return -Math.abs(num)
+
+	return num < 0 ? num : -num
+	
+	
+	// if (num > 0) {
+	// 	return -num
+	// } else {
+	// 	return num
+	// }
+}
+console.log(makeNegative(0))
