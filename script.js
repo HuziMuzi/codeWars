be10959c117d383228dacb6c8224d9ec88dd8400
HuziMuzi <laif17860@mail.ru>
2022-07-29 17:26:54 +0300

    done kata

--- script.js
+++ script.js
@@ -243,6 +243,35 @@ function areYouPlayingBanjo(name) {
     let letter = name.split('')
    return letter[0] ==='R' || letter[0] ==='r' ? `${name} plays banjo` :`${name} does not play banjo`
 }
-
-
 console.log(areYouPlayingBanjo('Rana'))
+
+
+const array1 = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ]
+
+//
+// function countSheeps(arrayOfSheep) {
+//     let count = 0
+//     for (let i = 0; i < arrayOfSheep.length; i++) {
+//         if ( arrayOfSheep[i] === true){
+//             count++
+//         }
+//         // arrayOfSheep[i] !== false || arrayOfSheep[i] !== null || arrayOfSheep[i] !== undefined ? count++ : ''
+//     }
+//     return count
+// }
+
+
+
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.filter(Boolean).length
+}
+
+
+console.log(countSheeps(array1))
+
+console.log(typeof Boolean)
